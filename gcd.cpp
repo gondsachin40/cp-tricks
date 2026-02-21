bd3c4957835436293e19c8c0c37a9c488d8d5212
gondsachin40 <162681936+gondsachin40@users.noreply.github.com>
2026-02-21 11:47:47 +0530

    Update gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -42,3 +42,86 @@ If u want an informal proof think of numbers in base 2 .We are calculating gcd's
 
 • An identity involving Euler's totient function: Gcd(a,b)=∑φ(k) where k are all common divisors of 'a' and 'b'
 */
+
+
+
+/*
+This relation is pretty interesting :)
+
+Important GCD properties we will use are
+
+gcd(a,b)=gcd(a±kb,b)=gcd(b,a%b)
+, such that a±kb≥0
+.
+gcd(a,0)=|a|
+ for a≠0
+ , since any number is a divisor of 0, and the greatest divisor of a
+ is |a|
+ .
+gcd(a1⋅a2,b)=gcd(a1,b)⋅gcd(a2,b)
+, if a1
+ and a2
+ are relatively prime.
+Let assume a>b
+, the base cases are F0=0
+, F1=1
+ and F2=1
+ .
+
+Claim I
+ : Every 2 consecutive Fibonacci numbers will always be co-prime gcd(Fa+1,Fa)=1
+.
+
+Proof I
+Claim II
+ : gcd(Fa,Fb)=gcd(Fa−b,Fb)
+
+Proof II
+Corollary from Claim II
+ : gcd(Fa,Fb)=gcd(Fb,Fa%b)
+ , from GCD property 1
+ we can keep on subtracting b
+ till a−kb≥0
+ which gives us a%b
+ at the end.
+
+Final Steps :
+
+Now we are seeing that subscript variables are following same pattern for finding gcd(a,b)=gcd(b,a%b)
+ recursively and indeed we do follow the same steps because of our proven Claims are allowing us do so.
+Just like gcd(a,b)
+ hit base case gcd(x,0)
+ and we claim x
+ is our Greatest Common Divisor, we will hit same base case for Fb,Fa%b
+ which will give, where x=gcd(a,b)
+
+gcd(Fa,Fb)=gcd(Fx,F0)=Fx=Fgcd(a,b)
+Fgcd(a,b)=gcd(Fa,Fb)
+which proves the interesting relation between gcd
+ and Fibonacci.
+
+
+Genralisation of 3rd one:
+    gcd(a*b , c) = gcd(gcd(a,c) * gcd(b ,c) , c)
+
+
+    GCD of an odd and an even number is NOT neccessarily 1. Take for example 6 and 15.
+
+Let n >= 1 We claim that gcd(n, n + 1) = 1
+
+We prove the claim via contradiction. Let gcd(n, n + 1) = r. It can be proved that r >= 1.
+
+Assume that r != 1. This implies that r > 1 since r >= 1.
+
+We can see that r | n and r | n + 1. This directly implies that there are integers x and y such that
+
+n = x * r and n + 1 = y * r.
+
+Subtracting both the equations, we get
+
+1 = r * (y — x)
+
+We can conclude from this equation that y — x = 1 and r = 1 since r >= 0
+
+This is a contradiction since r > 1. QED.
+*/
